3f9cf1bfa8ea5f6a2f1eb8adcaefc9719277ddde
Prashant <prashant@e-arth.in>
2016-03-15 09:11:48 +0000

    Card - 1045 : Added deals class with get all pipelines fn

---
+++ src/Api/Deals.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Fungku\HubSpot\Api;
+
+use Fungku\HubSpot\Exceptions\HubSpotException;
+
+class Deals extends Api
+{
+    
+    /**
+     * For a given portal, return all pipelines and their stages that have been created in the portal.
+     *
+     * A paginated list of pipelines will be returned to you, with a maximum of 100 contacts per page.
+     *
+     * @link http://developers.hubspot.com/docs/methods/deal-pipelines/get-all-deal-pipelines 
+     *
+     * @return mixed
+     */
+    public function all($params)
+    {
+        $endpoint = "/deals/v1/pipelines";
+
+        if (isset($params['property']) && is_array($params['property'])) {
+            $queryString = $this->generateBatchQuery('property', $params['property']);
+            unset($params['property']);
+        } else {
+            $queryString = null;
+        }
+
+        $options['query'] = $this->getQuery($params);
+
+        return $this->request('get', $endpoint, $options, $queryString);
+    }
+}
